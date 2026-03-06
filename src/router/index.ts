a5b928d8fd242a779586fa8a3446a421f11e4eb0
VarelaG25 <vjosemario38@gmail.com>
2026-03-06 02:06:29 -0600

    hashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+// src/router/index.ts
+import { createRouter, createWebHashHistory } from 'vue-router'
 
+// Rutas de la app
 const routes = [
   { path: '/', component: () => import('@/views/home/HomePage.vue') },
   { path: '/login', component: () => import('@/views/login/LoginPage.vue') },
   { path: '/register', component: () => import('@/views/register/RegisterPage.vue') },
-  {  path: '/sinisters', component: () => import('@/views/sinisters/SinistersPage.vue') },
-  {  path: '/reports', component: () => import('@/views/tracking/TrackingPage.vue') },
+  { path: '/sinisters', component: () => import('@/views/sinisters/SinistersPage.vue') },
+  { path: '/reports', component: () => import('@/views/tracking/TrackingPage.vue') },
 ]
 
+// Crear router usando hash history para GitHub Pages
 const router = createRouter({
-  history: createWebHistory('/MediCar/'),
+  history: createWebHashHistory('/medicar-landing/'), // <- usa hash para evitar 404
   routes,
 })
 
