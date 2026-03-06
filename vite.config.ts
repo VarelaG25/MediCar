1a182ee554e258594f215e05540f48e01f29ab3e
VarelaG25 <vjosemario38@gmail.com>
2026-03-06 01:44:31 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/medicar-landing/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
